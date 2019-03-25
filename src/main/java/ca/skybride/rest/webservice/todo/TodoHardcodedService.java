0f4691ac30a5e5952e6154cc0d26f8f4cfa4711d
sky <skybride@hotmail.com>
2019-03-25 11:37:36 -0400

    TodoHardcoded Service + TodoResource

---
+++ src/main/java/ca/skybride/rest/webservice/todo/TodoHardcodedService.java
@@ -0,0 +1,24 @@
+package ca.skybride.rest.webservice.todo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoHardcodedService {
+
+	private static List<Todo> todos = new ArrayList();
+	private static int idCounter = 0;
+	
+	static {
+		todos.add(new Todo(++idCounter, "sky", "Learn to Meow", new Date(), false ));
+		todos.add(new Todo(++idCounter, "sky", "Learn to Woof", new Date(), false ));
+		todos.add(new Todo(++idCounter, "sky", "Learn to Squeak", new Date(), false ));
+	}
+	
+	public List<Todo> findAll(){
+		return todos;
+	}
+}
